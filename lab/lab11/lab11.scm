4e9fd7d55adf6af366caaf30fb4a063c2c59abb2
mhyccc <1379888531@qq.com>
2024-11-28 17:57:54 +0800

    submit lab11 code & only finish Q1 Q2 Q3

---
+++ lab/lab11/lab11.scm
@@ -0,0 +1,23 @@
+(define (if-program condition if-true if-false)
+	(list 'if condition if-true if-false)
+)
+
+(define (square n) (* n n))
+
+(define (pow-expr base exp)
+	(cond
+		((= exp 0) 1)
+		; ((= exp 1) base)
+		((even? exp) `(square ,(pow-expr base (/ exp 2))))
+		((odd? exp) `(* ,base ,(pow-expr base (- exp 1))))
+	)
+)
+
+(define-macro (repeat n expr)
+  `(repeated-call ,n ___))
+
+; Call zero-argument procedure f n times and return the final result.
+(define (repeated-call n f)
+  (if (= n 1)
+      ___
+      (begin ___ ___)))
